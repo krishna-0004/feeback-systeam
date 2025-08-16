c72cbf6c6a56c73b18866afd975db962b2cd124b
Krishna Kadukar <krishnakadukar0004@gmail.com>
2025-08-16 21:31:00 +0530

    redy for render

--- Backend/api/index.mjs
+++
@@ -1,9 +0,0 @@
-import app from "../app.mjs";
-import serverless from "serverless-http";
-
-export const handler = serverless(app);
-
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-}
